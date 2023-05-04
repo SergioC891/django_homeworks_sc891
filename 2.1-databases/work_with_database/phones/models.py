261f184e9b9262bf2ddb2611f16cdb6a80064362
SergioC891 <sergiy.t81@gmail.com>
2023-05-05 00:58:56 +0300

    Hw#3: work_with_database changes

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=500)
+    price = models.FloatField()
+    image = models.TextField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,54 @@
 from django.shortcuts import render, redirect
-
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sort = request.GET.get('sort')
+    if sort == 'name':
+        phones_objects = Phone.objects.all().order_by('name')
+    elif sort == 'min_price':
+        phones_objects = Phone.objects.all().order_by('price')
+    elif sort == 'max_price':
+        phones_objects = Phone.objects.all().order_by('-price')
+    else:
+        phones_objects = Phone.objects.all()
+
+    phones = []
+    for phone in phones_objects:
+        phone_item = {
+            'name': phone.name,
+            'price': phone.price,
+            'image': phone.image,
+            'slug': phone.slug,
+        }
+        phones.append(phone_item)
+
     template = 'catalog.html'
-    context = {}
+    context = {
+        'phones': phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
+
+    phone_item = {}
+    if slug:
+        phone = Phone.objects.get(slug=slug)
+        if phone:
+            phone_item = {
+                'name': phone.name,
+                'price': phone.price,
+                'image': phone.image,
+                'release_date': phone.release_date,
+                'lte_exists': phone.lte_exists,
+            }
+
     template = 'product.html'
-    context = {}
+    context = {
+        'phone': phone_item
+    }
     return render(request, template, context)
